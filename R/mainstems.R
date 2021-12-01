e64ca8255e032441a213b9abd27315ee3c8014a2
David Blodgett <dblodgett@usgs.gov>
2021-12-01 07:54:55 -0600

    updates for service deployment

--- R/mainstems.R
+++ R/mainstems.R
@@ -9,7 +9,7 @@ make_mainstems <- function(ms_gpkg, out_f) {
                           (!is.na(outlet_GNIS_NAME) &  
                             totdasqkm > 50) | 
                             totdasqkm > 200) %>%
-    mutate(id = paste0("https://geoconnex.us/ref/mainstem/", LevelPathI), 
+    mutate(uri = paste0("https://geoconnex.us/ref/mainstems/", LevelPathI), 
            type = "https://www.opengis.net/def/schema/hy_features/hyf/HY_FlowPath",
            name_at_outlet = outlet_GNIS_NAME,
            name_at_outlet_gnis_id = outlet_GNIS_ID,
@@ -23,12 +23,14 @@ make_mainstems <- function(ms_gpkg, out_f) {
                                        paste0("https://geoconnex.us/nhdplusv2/huc12/", outlet_nhdpv2HUC12)),
            lengthkm = round(length, digits = 1),
            outlet_drainagearea_sqkm = round(totdasqkm, digits = 1)) %>%
-    select(id, type, name_at_outlet, name_at_outlet_gnis_id, head_nhdpv2_COMID, outlet_nhdpv2_COMID, 
+    select(id = LevelPathI, uri,
+           featuretype = type, name_at_outlet, name_at_outlet_gnis_id, head_nhdpv2_COMID, outlet_nhdpv2_COMID, 
            head_nhdpv2HUC12, outlet_nhdpv2HUC12, 
            lengthkm, outlet_drainagearea_sqkm,
            head_rf1ID, outlet_rf1ID, 
            head_nhdpv1_COMID, outlet_nhdpv1_COMID, 
-           head_latestHUC12, outlet_latestHUC12)
+           head_latestHUC12, outlet_latestHUC12) %>%
+    mutate(id = as.character(id))
     
   sf::write_sf(ms_out, out_f)
   
